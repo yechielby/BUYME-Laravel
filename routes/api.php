95e6ba9f19fa866e0a1094afd44a78d2acaa8a97
yechielby <yechielby@gmail.com>
2021-01-12 16:01:25 +0200

    add CORS Middleware

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,31 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'cors'], function() {
+
+    // --------------- [ User Controller Route] ------------
+    Route::post('user-registration', 'UserController@registerUser');
+    Route::post('user-login', 'UserController@loginUser');
+    // -------------------- [ Auth Tokens ]
+    Route::group(['middleware' => 'auth:api'], function () {
+
+        Route::get('user-detail', 'UserController@userDetail');
+
+        Route::post('update-user', 'UserController@update');
+
+        Route::delete('delete-user', 'UserController@destroy');
+
+        Route::post('create-task', 'TaskController@createTask');
+
+        Route::get('task-listing', 'TaskController@taskListing');
+
+        Route::get('task-detail/{task_id}', 'TaskController@taskDetail');
+
+        Route::post('share-task', 'TaskController@shareTask');
+
+        Route::post('update-task', 'TaskController@updateTask');
+
+        Route::delete('delete-task/{task_id}', 'TaskController@deleteTask');
+    });
+
 });
